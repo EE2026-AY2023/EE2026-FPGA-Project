8fc3886530789bcaa39e759a7f9451e5bc0f79fc
Koh Ngiap Hin <64647635+kohnh@users.noreply.github.com>
2023-03-31 17:34:38 +0800

    Lower count

--- sources_1/new/debounce.v
+++ sources_1/new/debounce.v
@@ -40,9 +40,9 @@ module clock_enable(input Clk_100M,output slow_clk_en);
     reg [26:0]counter=0;
     always @(posedge Clk_100M)
     begin
-       counter <= (counter>=25_000_000)?0:counter+1;
+        counter <= (counter>=20_000_000)?0:counter+1;
     end
-    assign slow_clk_en = (counter == 25_000_000)?1'b1:1'b0;
+    assign slow_clk_en = (counter == 20_000_000)?1'b1:1'b0;
 endmodule
 // D-flip-flop with clock enable signal for debouncing module 
 module my_dff_en(input DFF_CLOCK, clock_enable,D, output reg Q=0);
